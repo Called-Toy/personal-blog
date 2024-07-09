4f4ccdcc1b3bcac48c1be58a457440fe3170d213
Called_Toy <tangtao0312@126.com>
2024-07-09 18:13:53 +0800

    alter ip addtress

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%gdrvs#aablo#1yl(i^l+&16ui6h$pkl6kb74l^^sqbj*n&nk9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["198.168.1.2"]
+ALLOWED_HOSTS = ["192.168.1.2"]
 
 
 # Application definition
